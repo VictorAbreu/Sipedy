73e46da2b16abb19be92d6f812316a34625237ba
Victor Abreu <vlabreu@outlook.com>
2022-05-10 07:45:40 -0300

    first commit

---
+++ backend/src/main/java/br/com/vitt/sipedy/SipedyApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitt.sipedy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SipedyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SipedyApplication.class, args);
+	}
+
+}
