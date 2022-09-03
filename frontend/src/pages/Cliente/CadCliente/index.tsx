d632128b9467a2fe45adb87c4024ec876b79db4b
Victor Abreu <vlabreu@outlook.com>
2022-09-03 13:51:21 -0300

    [sipedy-16] - Finalizado layout barra de ações.

--- frontend/src/pages/Cliente/CadCliente/index.tsx
+++ frontend/src/pages/Cliente/CadCliente/index.tsx
@@ -1,15 +1,45 @@
 import './styles.css';
-
+import { ReactComponent as IconAddUser } from '../../../assets/images/addusuario.svg';
+import { ReactComponent as IconBack } from '../../../assets/images/voltar.svg';
+import { ReactComponent as IconClear } from '../../../assets/images/limpar.svg';
+import { ReactComponent as IconDel } from '../../../assets/images/excluir.svg';
+import { ReactComponent as IconSave } from '../../../assets/images/salvar.svg';
 
 const CadCliente = () => {
 
     return (
         <div className="cadcliente-page">
             <div className="base-card actionbar-container">
-                <h5>Cadastro de clientes</h5>
-            </div>
-            <div>
+                <div className="infobar-container">
+                    <IconAddUser className="bar-icon" />
+                    <h5>Cadastro de clientes</h5>
+                </div>
 
+                <div className="adduser-action">
+                    <div className="adduser-action-item">
+                        <button className="button-input">
+                            <IconBack className="bar-icon" />
+                            <h5>Voltar</h5>
+                        </button>
+                    </div>
+                    <div className="adduser-action-item">
+                        <button className="button-input">
+                            <IconClear className="bar-icon" />
+                            <h5>Limpar</h5>
+                        </button>
+                    </div>
+                    <div className="adduser-action-item">
+                        <IconDel className="bar-icon" />
+                        <h5>Excluir</h5>
+                    </div>
+                    <div className="adduser-action-item">
+                        <button className="button-input">
+                            <IconSave className="bar-icon" />
+                            <h5>Salvar</h5>
+                        </button>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
